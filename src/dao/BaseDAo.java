7d9a922c76d4bde00e240ab344c83dc7f7f85cfa
xhjdram <1205391574@qq.com>
2017-11-09 17:45:57 +0800

    no

--- src/dao/BaseDAo.java
+++ src/dao/BaseDAo.java
@@ -1,17 +1,22 @@
 package dao;
 
 import org.hibernate.criterion.DetachedCriteria;
-import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
 
 import java.io.Serializable;
 import java.util.List;
 
-public interface BaseDAo<T>  {
-    public void save(T t);
-    public void delet(T t);
-    public void delet(Serializable id);
-    public void update(T t);
-    public T getById(Serializable id  );
-    public Integer getTotalCount(DetachedCriteria detachedCriteria);
-    public List<T> getPageList(DetachedCriteria detachedCriteria,Integer start,Integer count);
+public interface BaseDAo<T> {
+    void save(T t);
+
+    void delet(T t);
+
+    void delet(Serializable id);
+
+    void update(T t);
+
+    T getById(Serializable id);
+
+    Integer getTotalCount(DetachedCriteria detachedCriteria);
+
+    List<T> getPageList(DetachedCriteria detachedCriteria, Integer start, Integer count);
 }
